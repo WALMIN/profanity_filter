e97e0f744385a1dd2fafe298ccef016c69c31dd2
Victor Bergene <v@walmin.com>
2023-04-25 00:07:26 +0200

    Added all the other languages (from LDNOOBW on GitHub) to the defaultWordsToFilterOutList

---
+++ lib/profanities/fil.dart
@@ -0,0 +1,16 @@
+List<String> fil = [
+  "puta ka",
+  "putang ina",
+  "tang ina",
+  "tangina",
+  "burat",
+  "bayag",
+  "bobo",
+  "nognog",
+  "tanga",
+  "ulol",
+  "kantot",
+  "anak ka ng puta",
+  "ulol",
+  "jakol"
+];
---
+++ lib/profanities/fr_CA_u_sd_caqc.dart
@@ -0,0 +1,9 @@
+List<String> fr_CA_u_sd_caqc = [
+  "noune",
+  "osti",
+  "criss",
+  "crisse",
+  "calice",
+  "tabarnak",
+  "viarge"
+];
