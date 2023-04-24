cf611b70abe98d57532bbf77af0071c18f782b2c
Victor Bergene <v@walmin.com>
2023-04-25 00:10:27 +0200

    Added word from another language to the example main

--- example/main.dart
+++ example/main.dart
@@ -7,18 +7,22 @@ void main() {
   //Other constructors are available, see [README.md](https://pub.dev/packages/profanity_filter).
   final filter = ProfanityFilter();
 
-  //This string contains the profanity 'ass'
-  String badString = 'You are an ass';
+  //These strings contains the profanities 'ass' in two languages
+  tryProfanityFilter("(EN) You are an ass", filter);
+  tryProfanityFilter("(SV) Du är en röv", filter);
+  tryProfanityFilter("(EN & SV) Din röv is an ass", filter);
+}
 
+void tryProfanityFilter(String text, ProfanityFilter filter) {
   //Check for profanity - returns a boolean (true if profanity is present)
-  bool hasProfanity = filter.hasProfanity(badString);
-  print('The string $badString has profanity: $hasProfanity');
+  bool hasProfanity = filter.hasProfanity(text);
+  print('The string $text has profanity: $hasProfanity');
 
   //Get the profanity used - returns a List<String>
-  List<String> wordsFound = filter.getAllProfanity(badString);
+  List<String> wordsFound = filter.getAllProfanity(text);
   print('The string contains the words: $wordsFound');
 
   //Censor the string - returns a 'cleaned' string.
-  String cleanString = filter.censor(badString);
-  print('Censored version of "$badString" is "$cleanString"');
+  String cleanString = filter.censor(text);
+  print('Censored version of "$text" is "$cleanString"\n');
 }
